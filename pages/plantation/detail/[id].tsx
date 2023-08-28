7ed942d46381d3a33cdede1ccbbdf1bb929d0c76
surif-yandi <surif.y4ndi@gmail.com>
2023-08-28 16:34:17 +0700

    add page detail plantation

---
+++ pages/plantation/detail/[id].tsx
@@ -0,0 +1,83 @@
+import Link from "next/link"
+import { useRouter } from "next/router"
+
+export default function DetailPlantation() {
+    const router = useRouter()
+
+    type dataType = {
+        id : number,
+        date : string,
+        image : string,
+        caption : string
+    }
+
+    const datas = [
+        {
+            id : 1,
+            date : "28 Agustus 2023",
+            image : "/img/index/bg-hero-banner.webp",
+            caption : "Lorem ipsum dolor sit amet consetetur adipisicing elit."
+        },
+        {
+            id : 2,
+            date : "1 September 2023",
+            image : "/img/index/bg-hero-banner.webp",
+            caption : "Lorem ipsum dolor sit amet consetetur adipisicing elit."
+        },
+        {
+            id : 3,
+            date : "10 September 2023",
+            image : "/img/index/bg-hero-banner.webp",
+            caption : "Lorem ipsum dolor sit amet consetetur adipisicing elit."
+        },
+    ]
+    return (
+        <>
+            <div className="breadcrumb">
+                <div className="container">
+                    <h2>Detail Plantation</h2>
+                    <ul>
+                        <li><Link href="/">Home</Link></li>
+                        <li><Link href="/plantation">Plantation</Link></li>
+                        <li className="active"><Link href={`/plantation/detail/${router.query.id}`}>Detail Plantation</Link></li>
+                    </ul>
+                </div>
+            </div>
+            <main className="main-wrapper">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-lg-7 plantation-detail mb-5 mb-lg-0">
+                            <div className="plantation-detail--inner">
+                                <h1 className="plantation-detail--title">PT. Alpha Company</h1>
+                                <ul className="plantation-detail--meta">
+                                    <li><strong>Jumlah Bibit :</strong> 5.000 bibit</li>
+                                    <li><strong>Alamat Penamaman :</strong> Kecamatan Sungai Pinyuh, Kabupaten Mempawah</li>
+                                </ul>
+                                <div className="map-google">
+                                    <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d31918.529806852664!2d109.36383093005371!3d-0.058708350541439414!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sid!4v1693154364266!5m2!1sen!2sid" width="100%" height="500" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-lg-4 offset-lg-1">
+                            <h3 className="mb-4">Progress Penamaman Bibit</h3>
+                            <ul className="timeline-progress">
+                                {
+                                    datas &&
+                                    datas.map((item:dataType) => {
+                                        return (
+                                            <li>
+                                                <span className="label label__main progress--date">{item.date}</span>
+                                                <img src={`${item.image}`} alt="" />
+                                                <p>{item.caption}</p>
+                                            </li>
+                                        )
+                                    })
+                                }
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </main>
+        </>
+    )
+}
