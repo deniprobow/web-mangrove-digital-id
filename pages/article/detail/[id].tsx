f4fbac5e28f37f5e701170b98b6d18d1fc3e5878
surif-yandi <surif.y4ndi@gmail.com>
2023-09-26 15:29:49 +0700

    implement API detail article

--- pages/article/detail/[id].tsx
+++ pages/article/detail/[id].tsx
@@ -4,18 +4,10 @@ import { Breadcrumb } from '@/components/breadcrumb/style'
 import { useRouter } from 'next/router'
 import { faUser, faCalendar } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import useSWR from 'swr'
 
 export default function DetailArticle() {
     const router = useRouter()
-
-    type dataType = {
-        id : number,
-        title : string,
-        date : string,
-        writer : string,
-        img : string,
-        text : string,
-    }
 
     const breadcrumbLinks = [
         {
@@ -30,21 +22,17 @@ export default function DetailArticle() {
         }
     ]
 
-    const datas = [
-        {
-            id : 1,
-            title : 'The Importance of Mangroves in Coastal Ecosystems',
-            date : '15 September 2023',
-            writer : 'Surif Yandi',
-            img : '/img/album/album-cover-4.webp',
-            text : 'Mangroves are a vital component of coastal ecosystems worldwide, providing a wide range of ecological and economic benefits. These unique coastal forests thrive in the transition zone between land and sea, where they endure the challenges of saltwater, tides, and often harsh environmental conditions. In this article, we explore the significance of mangroves and their crucial roles in maintaining the health of coastal environments.'
-        }
-    ]
+    const fetcher = async (url:string) => fetch(url).then((res) => res.json())
+
+    const { data, error, isLoading } = useSWR(`${process.env.NEXT_PUBLIC_SERVER_HOST}/articles/${router.query.id}`, fetcher)
+
+    if(isLoading) return <Loading />
+
+    console.log(data)
 
     return (
         <>
             <Header isHomePage={false} />
-            <Loading />
             <Breadcrumb
                 title = "Detail Article"
                 links = {breadcrumbLinks}
@@ -53,28 +41,15 @@ export default function DetailArticle() {
                 <div className="container">
                     <div className="row justify-content-center">
                         <div className="col-lg-7">
-                            {
-                                datas &&
-                                datas.map((data:dataType) => {
-                                    return (
-                                        <>
-                                            <h2 className="mb-4">{data.title}</h2>
-                                            <ul className="single-post--meta mb-4">
-                                                <li>
-                                                    <FontAwesomeIcon icon = {faCalendar} />
-                                                    <span>{data.date}</span>
-                                                </li>
-                                                <li>
-                                                    <FontAwesomeIcon icon = {faUser} />
-                                                    <span>{data.writer}</span>
-                                                </li>
-                                            </ul>
-                                            <img className="img-fluid mb-4" src={data.img} alt="" />
-                                            <p>{data.text}</p>
-                                        </>
-                                    )
-                                })
-                            }
+                            <h2 className="mb-4">{data.article_title}</h2>
+                            <ul className="single-post--meta mb-4">
+                                <li>
+                                    <FontAwesomeIcon icon = {faCalendar} />
+                                    <span>{data.created_at.split('T')[0]}</span>
+                                </li>
+                            </ul>
+                            <img className="img-fluid mb-4" src={data.article_image} alt="" />
+                            <div>{data.article_content}</div>
                         </div>
                     </div>
                 </div>
